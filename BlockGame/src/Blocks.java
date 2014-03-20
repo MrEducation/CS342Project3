282ce48ae8d24f4faa441fc21f1b578bd352bb8e
Dominick Rauba <dominickrauba@yahoo.com>
2014-03-20 15:23:27 -0500

    Made Blocks class. Made vertical and horizontally slidding Blocks

--- BlockGame/src/Blocks.java
+++ BlockGame/src/Blocks.java
@@ -1,4 +1,109 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.lang.*;
 
-public class Blocks {
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
+public class Blocks extends JButton implements MouseInputListener
+{
+		private char direction;
+		private String name;
+		private JButton button;
+		private int xLoc;
+		private int yLoc;
+		private int width;
+		private int height;
+		
+
+		Blocks(char direction, String name, int xLoc, int yLoc, int width, int height)
+		{
+			this.direction = direction;
+			this.name = name;
+			setButton(new JButton(name));
+			button.setBounds(xLoc*100,yLoc*100,width*100,height*100);
+			button.setName(name);
+
+		}
+		
+		
+		public char getDirection()
+		{
+			return direction;
+		}
+		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public void setDirection(char direction)
+		{
+			this.direction = direction;
+		}
+		
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		public JButton getButton() {
+			return button;
+		}
+
+
+		public void setButton(JButton button) {
+			this.button = button;
+		}
+		
 }
--- BlockGame/src/Game.java
+++ BlockGame/src/Game.java
@@ -10,42 +10,39 @@ import java.util.ArrayList;
 
 public class Game extends JFrame implements MouseInputListener
 {
- private JButton objButtonDrag;
- private JButton objButtonDrag1;
  public int pressed;
  public int released;
  public JButton button; 
+ public Blocks block; 
+ 
+ private ArrayList<Blocks> blocks;
      
      public Game()
      {
+    	 
           super("Game");
           
-          ArrayList<JButton> blocks = new ArrayList<JButton>();
-        		  
-          objButtonDrag = new JButton("Horizontal");
-          objButtonDrag.addMouseListener(this);
-          objButtonDrag.addMouseMotionListener(this);
-           objButtonDrag.setBounds(0, 100, 100, 100);
-           
-           objButtonDrag1 = new JButton("Vertical");
-           objButtonDrag1.addMouseListener(this);
-           objButtonDrag1.addMouseMotionListener(this);
-           objButtonDrag1.setBounds(100, 0, 100, 100);
-
+          blocks = new ArrayList<Blocks>();
+        
           getContentPane().setLayout(null);
+          setSize(new Dimension(600, 625));
           
-          blocks.add(objButtonDrag);
-          blocks.add(objButtonDrag1);
+          blocks.add(new Blocks('h', "Horizontal", 1, 1, 1,1));
+          blocks.add(new Blocks('u', "Vertical", 2, 2, 1,2));
           
-          for(JButton b : blocks)
+          for(Blocks b : blocks)
           {
-          getContentPane().add(b);
-          }
 
-          setSize(new Dimension(617, 617));
+        	  getContentPane().add(b.getButton());
+			  b.getButton().addMouseListener(this);
+	          b.getButton().addMouseMotionListener(this);
+	          
+	        
+          }
        
 
           setVisible(true);
+          setResizable(false);
 
           addWindowListener(new WindowAdapter() {
                public void windowClosing(WindowEvent argEvent) {
@@ -58,32 +55,76 @@ public class Game extends JFrame implements MouseInputListener
      public void mousePressed(MouseEvent argEvent) {
     //	 System.out.println("Pressed: " + argEvent.getX());
     	 button = (JButton) argEvent.getSource();
-    	 pressed = argEvent.getX();
+
+    	 for(Blocks b : blocks)
+         {
+       	  if(b.getName().equals(button.getName()))
+       	  {
+       	  	block = b;
+       	  }
+         }
+    	 
+
+    	 
+    	 if(block.getDirection() == 'h')
+    	 {
+    	    pressed = argEvent.getX();
+    	 }
+    	 else if(block.getDirection() == 'u')
+    	 {
+    		pressed = argEvent.getY();
+    	 }
 
     	 
  	}
      
      public void mouseReleased(MouseEvent argEvent) {
   
-    	 System.out.println("width: " + getWidth());
-    	 released = argEvent.getX();
+
     	 
-    	 if(pressed > released) //move button to left
-    	 {
-    		 Rectangle objBounds = button.getBounds();
-    		 if((int)button.getLocation().getX() >= (int)button.getWidth())
-    			 button.setLocation(objBounds.x-100, 100);
-    	 }
+    	 if(block.getDirection() == 'h')
+    		 released = argEvent.getX();
+    	 else if(block.getDirection() == 'u')
+     		 released = argEvent.getY();
+
+    	 if(block.getDirection() == 'h'){
+    		 if(pressed > released) //move button to left
+    		 {
+    			 Rectangle objBounds = button.getBounds();
+    			 if((int)button.getLocation().getX() != 0)
+    				 button.setLocation(objBounds.x-100, (int)button.getLocation().getY());
+    		 }
     	 
-    	 else if(pressed < released) // move button to right
-    	 {
+    		 else if(pressed < released) // move button to right
+    		 {
     		 
-    		 Rectangle objBounds = button.getBounds();
-    		if((int)button.getLocation().getX() < getWidth()-((int)button.getWidth()+17))
-    			button.setLocation(objBounds.x+100, 100);
+    			 Rectangle objBounds = button.getBounds();
+    			 if((int)button.getLocation().getX() < getWidth()-((int)button.getWidth()+17))
+    				 button.setLocation(objBounds.x+100, (int)button.getLocation().getY());
+    		 }
+    	 
+    		 System.out.println("Location: (" + (int)button.getLocation().getX()+"," + (int)button.getLocation().getY() + ")");
     	 }
     	 
-    	 System.out.println("Location: " + (int)button.getLocation().getX());
+    	 else if(block.getDirection() == 'u'){
+    		 if(pressed > released) //move button to left
+    		 {
+    			 Rectangle objBounds = button.getBounds();
+    			 if((int)button.getLocation().getY() != 0)
+    				 button.setLocation((int)button.getLocation().getX(), objBounds.y-100);
+    		 }
+    	 
+    		 else if(pressed < released) // move button to right
+    		 {
+    		 
+    			 Rectangle objBounds = button.getBounds();
+    			 if((int)button.getLocation().getY() < getWidth()-((int)button.getHeight()+17))
+    				 button.setLocation((int)button.getLocation().getX(), objBounds.y+100);
+    		 }
+    	 
+    		 System.out.println("Location: (" + (int)button.getLocation().getX()+"," + (int)button.getLocation().getY() + ")");
+    	 }
+    	 
      }
  
 
